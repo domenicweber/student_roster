be2f6310f64a0d90075fb52cc7fc576c2517afe2
Domenic Weber <domenic.weber@gmail.com>
2015-07-22 10:38:19 -0600

    Updates studnets.rb validation Fixes phone validation Removes duplication
    [#99003474]

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,16 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "WHY YOU DUMB BASTARD")
+    end
+  end
+end
+
+
 class Student < ActiveRecord::Base
+  validates :email, presence: true, email: true
+  validates :cell_phone, :home_phone, :work_phone, format: {
+      with: /\d{3}?\d{3}?\d{4}/,
+      message: "Bad Dog!, this number isn't legit"
+    }
 end
